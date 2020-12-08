6f5aa5a656c38d5be63a67f111f7850fa3fe9289
BrenoAccioly <breno.accioly91@gmail.com>
2020-12-08 20:39:14 +0000

    Added url parse method

--- download/download.c
+++ download/download.c
@@ -4,32 +4,28 @@
 #include <netdb.h> 
 #include <sys/types.h>
 #include <netinet/in.h> 
-#include<arpa/inet.h>
+#include <arpa/inet.h>
+
+#include <string.h>
 
 #define SERVER_PORT 21
 
-int main(int argc, char *argv[])
-{
+struct hostent* get_ip(char *hostname){
 	struct hostent *h;
-
-        if (argc != 2) {  
-            fprintf(stderr,"usage: getip address\n");
-            exit(1);
-        }
-        
-/*
-struct hostent {
-	char    *h_name;	Official name of the host. 
+	/*
+	struct hostent {
+		char    *h_name;	Official name of the host. 
     	char    **h_aliases;	A NULL-terminated array of alternate names for the host. 
-	int     h_addrtype;	The type of address being returned; usually AF_INET.
+		int     h_addrtype;	The type of address being returned; usually AF_INET.
     	int     h_length;	The length of the address in bytes.
-	char    **h_addr_list;	A zero-terminated array of network addresses for the host. 
+		char    **h_addr_list;	A zero-terminated array of network addresses for the host. 
 				Host addresses are in Network Byte Order. 
-};
+	};
 
-#define h_addr h_addr_list[0]	The first address in h_addr_list. 
-*/
-    if ((h=gethostbyname(argv[1])) == NULL) {  
+	#define h_addr h_addr_list[0]	The first address in h_addr_list. 
+	*/
+
+    if ((h=gethostbyname(hostname)) == NULL) {  
             herror("gethostbyname");
             exit(1);
     }
@@ -37,11 +33,77 @@ struct hostent {
     printf("Host name  : %s\n", h->h_name);
     printf("IP Address : %s\n",inet_ntoa(*((struct in_addr *)h->h_addr)));
 
+	return h;
+}
 
+void parse_url(char *url, char *user, char *pwd, char *host, char *url_path){
+
+	if(strncmp(url, "ftp://", 6) != 0){
+		fprintf(stderr,"Error: url should start with ftp:// \n");
+		exit(0);
+	}
+	
+	// url without ftp://
+	char *_url = malloc(strlen(url) - 6);
+	memcpy(_url, url + 6, strlen(url) - 6);
+	
+	char *_url_cpy = malloc(strlen(url) - 6);
+	
+	// Get user and password if defined
+	if(strchr(_url, '@') != NULL){
+		strcpy(_url_cpy, _url);
+		strtok(_url_cpy, "@");
+
+		strcpy(user, strtok(_url_cpy, ":")); 
+		strcpy(pwd, strtok(NULL, ":"));
+		
+		// Get host
+		strcpy(_url_cpy, _url);
+		strtok(_url_cpy, "@"); 
+		_url_cpy = strtok(NULL, "@");
+	
+		strcpy(host, strtok(_url_cpy, "/"));
+	
+		// Get url_path
+		memcpy(url_path, _url  + strlen(user) + strlen(pwd) + 2 + strlen(host), strlen(_url) - strlen(host));
+	}
+	else{
+		user[0] = '\0';
+		pwd[0] = '\0';
+		// Get host
+		strcpy(_url_cpy, _url);
+		strcpy(host, strtok(_url_cpy, "/"));
+	
+		// Get url_path
+		memcpy(url_path, _url  + strlen(host), strlen(_url) - strlen(host));
+	}
+	
+	printf("user: %s\n", user);
+	printf("pwd: %s\n", pwd);
+	printf("host: %s\n", host);
+	printf("url-path: %s\n", url_path);
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {  
+        fprintf(stderr,"usage: download ftp://[<user>:<password>@]<host>/<url-path> \n");
+        exit(1);
+    }
+	
     int sockfd;
 	struct sockaddr_in server_addr;
-	char buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";
 	int bytes;
+
+	char *user     = (char*)malloc(256); // optional
+	char *pwd      = (char*)malloc(256); // optional
+	char *host     = (char*)malloc(256);
+	char *url_path = (char*)malloc(256);
+
+	parse_url(argv[1], user, pwd, host, url_path);
+
+	struct hostent *h = get_ip(host);
+
 	/*server address handling*/
 
 	bzero((char *)&server_addr, sizeof(server_addr));
@@ -65,9 +127,16 @@ struct hostent {
 		exit(0);
 	}
 
-    /*send a string to the server*/
-	bytes = write(sockfd, buf, strlen(buf));
+	// TODO login host
+
+	write(sockfd, "user ", 5);
+	bytes = write(sockfd, "rcom", strlen("rcom"));
 	printf("Bytes escritos %d\n", bytes);
+	
+	// TODO read server reply
+	char r[100];
+	read(sockfd, r, 100);
+	printf("Reply: %s\n", r);
 
 	close(sockfd);
 	exit(0);
