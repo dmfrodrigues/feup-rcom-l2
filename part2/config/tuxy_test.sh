b27ec0a1e58407f6936eb01a29b13cd8a24aaf86
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-12-14 15:52:31 +0000

    Changed pings

--- part2/config/tuxy_test.sh
+++ part2/config/tuxy_test.sh
@@ -6,17 +6,17 @@ if [ -z "$1" ]; then
 fi
 y=$1
 
-ping 172.16.${y}0.1    # tuxy3 VLAN y0
-ping 172.16.${y}0.254  # tuxy4 VLAN y0
-ping 172.16.${y}1.253  # tuxy4 VLAN y1
-ping 172.16.${y}1.1    # tuxy2 VLAN y1
-ping 172.16.${y}1.254  # Rc inside
-ping 172.16.1.${y}9    # Rc outside
+ping -c 3 172.16.${y}0.1    # tuxy3 VLAN y0
+ping -c 3 172.16.${y}0.254  # tuxy4 VLAN y0
+ping -c 3 172.16.${y}1.253  # tuxy4 VLAN y1
+ping -c 3 172.16.${y}1.1    # tuxy2 VLAN y1
+ping -c 3 172.16.${y}1.254  # Rc inside
+ping -c 3 172.16.1.${y}9    # Rc outside
 
-ping google.com
-ping example.com
-ping example.org
-ping www.sapo.pt
+ping -c 3 google.com
+ping -c 3 example.com
+ping -c 3 example.org
+ping -c 3 www.sapo.pt
 
 make -C download
 make -C download test_all
